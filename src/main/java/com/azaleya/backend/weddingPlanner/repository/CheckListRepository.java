692ef653444d620dc09119e5fdff5614741dcebd
toydala99 <antoniodala83@gmail.com>
2023-07-24 10:24:06 +0100

    READ ME

--- src/main/java/com/azaleya/backend/weddingPlanner/repository/CheckListRepository.java
+++ src/main/java/com/azaleya/backend/weddingPlanner/repository/CheckListRepository.java
@@ -4,6 +4,9 @@ import com.azaleya.backend.weddingPlanner.entites.CheckList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CheckListRepository extends JpaRepository<CheckList, String> {
+
 }
