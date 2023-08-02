ca3f6bd679d9977b46456df716cfdf9ebafe4177
Calime Silva <calimesilva1999@gmail.com>
2023-08-02 12:36:51 +0100

    Supplier complete

---
+++ src/main/java/com/azaleya/backend/ecommerce/Repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.azaleya.backend.ecommerce.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.azaleya.backend.ecommerce.entities.Supplier;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+}
---
+++ src/main/java/com/azaleya/backend/ecommerce/services/SupplierServices.java
@@ -0,0 +1,65 @@
+package com.azaleya.backend.ecommerce.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.azaleya.backend.ecommerce.Repository.SupplierRepository;
+import com.azaleya.backend.ecommerce.dto.SupplierDTO;
+import com.azaleya.backend.ecommerce.entities.Supplier;
+import com.azaleya.backend.weddingPlanner.services.exception.ResourceNotFoundException;
+
+@Service
+public class SupplierServices {
+	@Autowired
+	SupplierRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<SupplierDTO>FindPage(PageRequest pageRequest){
+		Page<Supplier>list=repository.findAll(pageRequest);
+		return list.map(x->new SupplierDTO(x));
+	}
+	
+	@Transactional
+	public SupplierDTO saveSupplier(SupplierDTO supplier) {
+		Supplier entity = new Supplier();
+		entity.setNome(supplier.getNome());
+		entity.setEmail(supplier.getEmail());
+		entity.setImgPerfilUrl(supplier.getImgPerfilUrl());
+		entity.setNif(supplier.getNif());
+		entity.setPass(supplier.getPass());
+		entity= repository.save(entity);
+		return new SupplierDTO(entity);
+	}
+	
+	@Transactional
+	public SupplierDTO updteSupplier(SupplierDTO dto, Long id) {
+		try {
+			Supplier findId= repository.getOne(id);
+			findId.setEnderecos(dto.getEnderecos());
+			findId.setNome(dto.getNome());
+			findId.setPass(dto.getPass());
+			findId.setNif(dto.getNif());
+			findId.setEmail(dto.getEmail());
+			findId.setImgPerfilUrl(dto.getImgPerfilUrl());
+			findId=repository.save(findId);
+			return new SupplierDTO(findId);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Erro ao encontrar este fornecedor");
+		}
+		
+	}
+	@Transactional
+	public void deleteSupplier(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Naõ podes deletar um fornecedor que na existite");
+		}
+	}
+	
+}
