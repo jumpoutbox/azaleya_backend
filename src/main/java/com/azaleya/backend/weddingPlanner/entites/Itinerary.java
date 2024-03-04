db68739c263f65e556c116a603dbc37084382e2c
toydala99 <antoniodala83@gmail.com>
2024-03-04 14:59:05 +0100

    updates

---
+++ src/main/java/com/azaleya/backend/weddingPlanner/entites/Itinerary.java
@@ -0,0 +1,15 @@
+package com.azaleya.backend.weddingPlanner.entites;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_itinerary")
+public class Itinerary implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID )
+    private String id;
+}
